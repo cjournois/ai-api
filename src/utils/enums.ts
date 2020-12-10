697cb6fda6ecd2eaecf157a3a8995e6f6373fb37
cjournois <cjournois@assist-evolution.com>
2020-12-10 23:06:35 +0100

    feat(models): add IAnimal and IPrediction

--- src/utils/enums.ts
+++ src/utils/enums.ts
@@ -1,3 +1,12 @@
 export const CollectionName = {
+	Animals: 'animals',
 	Charlies: 'charlies',
+	Predictions: 'predictions',
+}
+
+export const Animal = {
+	Bear: 'bear',
+	Cat: 'cat',
+	Dog: 'dog',
+	Lion: 'lion',
 }
